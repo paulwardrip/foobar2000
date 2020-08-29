379e50f4dd0e5d332aa5d5762041cb3fc32fe8b1
Paul Wardrip <pwardrip@gmail.com>
2020-08-29 06:02:12 -0400

    setup project

---
+++ component/ESLyric/scripts/interface.js
@@ -0,0 +1,39 @@
+//===================================================
+//=========Sample Lyric Source For ESLyric===========
+//========See "Tools->Readme" for more info==========
+//===================================================
+
+
+function get_my_name()
+{
+    return "Genius.com";
+}
+
+function get_version()
+{
+    return "0.0.0";
+}
+
+function get_author()
+{
+    return "Anonymous";
+}
+
+function start_search(info,callback)
+{
+    //add your codes here
+
+    /*
+    var title = info.Title;
+    var artist = info.Artist;
+
+    //search...
+
+    //add new lyrics to eslyric
+    var new_lyric = callback.CreateLyric();
+    new_lyric.Title = "Lyric title"; // lyric title
+    new_lyric.Artist = "Lyric artist";
+    new_lyric.LyricText = "!!!!LYRIC CONTENT!!!!!!!";
+	callback.AddLyric(new_lyric);
+    */
+}
